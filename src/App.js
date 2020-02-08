44e68c9486d6481207d7857b45a0bd4dc9083c64
ATarsa <agnieszkatarsa@gmail.com>
2020-02-08 16:14:12 +0000

    Initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SelectComponent from './components/SelectComponent'
 
 function App() {
+  const display = 'Mechanism'
+  const options = ['Abrasion', 'Blunt', 'Burn', 'Frost', 'Gun Shot', 'Laceration', 'RTC', 'Stab' ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SelectComponent 
+      display={display}
+      options={options} />
   );
 }
 
