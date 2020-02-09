99aab359609b94be208f547703ce61d4f0bf0ac3
ATarsa <agnieszkatarsa@gmail.com>
2020-02-09 19:45:28 +0000

    Updates

---
+++ precache-manifest.11037911aa21fb5ca5eae46171f9186e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2863a2d65d8bc8281fe46e440a908a47",
+    "url": "/display-component/index.html"
+  },
+  {
+    "revision": "05bfb3c976092a9f77ab",
+    "url": "/display-component/static/css/main.62c6cd90.chunk.css"
+  },
+  {
+    "revision": "4fa72ca33e704a19d11a",
+    "url": "/display-component/static/js/2.02357cc8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/display-component/static/js/2.02357cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bfb3c976092a9f77ab",
+    "url": "/display-component/static/js/main.bc2199d8.chunk.js"
+  },
+  {
+    "revision": "b8c4eaf7363b2ddf411b",
+    "url": "/display-component/static/js/runtime-main.b21ea4b9.js"
+  }
+]);
